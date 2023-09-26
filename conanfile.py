8dbf05773147574ccd44359678824717a85b678a
Sileno Brito <silenobrito@gmail.com>
2023-09-26 14:39:38 -0300

    alterado caminho do cmake module path

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,49 @@
-#
+# ------------------------------------------------------------------------------
+# conanfile.py for resiprocate project
 # Created by Sileno de Oliveira Brito on 22/09/2023.
-# Copyright (c) 2023 Nerd4ever All rights reserved.
+# -----------------------------------------------------------------------------
+# In this `conanfile.py`, we're adopting certain naming conventions for options:
 #
-
+# - The prefix `with_` is used to denote items that rely on external libraries.
+# - The prefix `enable_` is employed to activate functionalities that don't
+#   modify external libraries and to include additional binaries from other projects.
+# - Elements are kept without a prefix, following a standard convention that's
+#   well-known across many packages.
+#
+# As this is the initial version of these conventions, they might undergo
+# changes throughout the project's development.
+#
+# The reasoning behind the `with_` prefix is to bring heightened awareness
+# when utilizing external libraries. It aids in identifying both external
+# dependencies and potential licensing changes. The adoption of certain licenses,
+# such as GPL, can influence the overall project license. We aim to ensure clarity
+# when evaluating library combinations. For instance, with Conan, it's simple to
+# swap a static library for a shared one. In the scenario of an LGPLv3 library,
+# this could inadvertently affect the licensing aspect of the project.
+# -----------------------------------------------------------------------------
+# Note: This Conan recipe is licensed under the MIT License.
+# The resiprocate project and its associated components may be licensed differently.
+# Please see the project's root license for those details.
+#
+# MIT License:
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
 import os
 import glob
 import shutil
@@ -11,7 +52,6 @@ from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import get, chdir
 
 required_conan_version = ">=2.0.0"
-
 
 class ResiprocateConan(ConanFile):
     build_policy = "missing"
